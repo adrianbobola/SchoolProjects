0f3d7881ac75c86cc0293c7078b4183300c1a355
adrianbobola <adrian.bobola@gmail.com>
2020-11-27 19:04:14 +0100

    1
    1

---
+++ test/main.c
@@ -0,0 +1,17 @@
+#include "parser.h"
+#include "codegen.h"
+#include "scanner.h"
+
+int main()
+{
+    int scanner_file_return = set_file(stdin);
+    if (scanner_file_return == -1)
+    {
+        fprintf(stderr, "Failed to open file");
+        return 99;
+    }
+
+    set_code_output(stdout);
+    int parse_return = parse();
+    return parse_return;
+}
